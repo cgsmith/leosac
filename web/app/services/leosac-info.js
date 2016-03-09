f8c3f08275e0f2f2782b432cf5e7c3467bb46f56
xaqq <kapp.arno@gmail.com>
2016-03-09 16:10:53 +0100

    Refactor templates for web interface.

--- web/app/services/leosac-info.js
+++ web/app/services/leosac-info.js
@@ -8,6 +8,17 @@ export default Ember.Service.extend({
   websocket: Ember.inject.service(),
   version: undefined,
 
+  /**
+   * The version number of the Leosac server.
+   */
+  version: "0.0.0",
+
+  /**
+   * This title of the current view.
+   * Not sure this is the best way to manage this...
+   */
+  current_view_title: "Default",
+
   init()
   {
     "use strict";
